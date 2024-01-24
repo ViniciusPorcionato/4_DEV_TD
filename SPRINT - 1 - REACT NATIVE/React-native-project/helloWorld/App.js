f009885d8f9e31b2172e856fee483be5fb3912dc
Vinicius Porcionato <porcionatovinicius@gmail.com>
2024-01-24 17:21:49 -0300

    Introdução ao React Native: Hello World~

---
+++ SPRINT - 1 - REACT NATIVE/React-native-project/helloWorld/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World !</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 50,
+    borderWidth : 2,
+    borderRadius: 15,
+    padding: 10,
+    backgroundColor: 'grey',
+    color: 'white'
+    
+  }
+});
